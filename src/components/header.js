342bd48dc665afdf696431b8662daa0e0f1eca8f
isadamascena <idamasce@thoughtworks.com>
2018-11-16 12:06:46 -0300

    using dynamic data

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+const user = {
+    name: 'Isa',
+    lastname: 'Damascena',
+    age: 24
+}
 const Header = () => {
-    return <div>Header</div>
+    return (
+        <div>
+            <div>{user.name}</div>
+            <div>{user.lastname}</div>
+            <div>{user.age}</div>
+        </div>
+    );
 }
 
 export default Header;
